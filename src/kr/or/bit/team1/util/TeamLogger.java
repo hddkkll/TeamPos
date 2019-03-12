62eee56dce45a71d7f32765599111156b6cea257
rakjido <ndr.zenn@gmail.com>
2019-03-12 20:39:47 +0900

    TeamFiles, TeamLogger frame

---
+++ src/kr/or/bit/team1/util/TeamLogger.java
@@ -0,0 +1,13 @@
+package kr.or.bit.team1.util;
+
+public class TeamLogger {
+	String path="";
+	
+	public static void getLogger(String path) {
+		
+	}
+	
+	public static void info(String contents) {
+		
+	}
+}
